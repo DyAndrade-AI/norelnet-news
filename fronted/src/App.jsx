4135c4d611e50ef5b020642f543cd361c0e30f10
AndresRelu <andres.romero.vired@gmail.com>
2025-11-23 13:02:28 -0600

    refactor: simplificar frontend - remover contenido de restaurant
    - Simplificar Navbar: remover autenticación, menú de restaurant, búsqueda
    - Actualizar App.jsx: solo rutas de noticias (/, /news, /article/:id)
    - Cambiar logo a 'Norelnet News'
    - Remover EditorFab, Home de restaurant, Menu, Reservations
    - Dejar solo lo esencial: listado y detalle de noticias

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,13 +1,9 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Menu from "./pages/Menu";
-import Reservations from "./pages/Reservations";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NewsList from "./pages/NewsList";
 import ArticleDetail from "./pages/ArticleDetail";
 import Navbar from "./components/Navbar";
 import "./index.css";
-import EditorFab from "./components/EditorFab";
 
 export default function App() {
   return (
@@ -15,16 +11,11 @@ export default function App() {
       <div className="app">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/" element={<NewsList />} />
           <Route path="/news" element={<NewsList />} />
           <Route path="/article/:id" element={<ArticleDetail />} />
-          {/* más rutas aquí */}
         </Routes>
       </div>
-      {/* Controles de colaboración para el staff */}
-      <EditorFab />
     </BrowserRouter>
   );
 }
